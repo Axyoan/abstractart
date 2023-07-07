1d7b64680fc0c41485d5d68a41bdbdab1804d2fb
Axyoan Marcelo Castillo <axyoan.marcelo@alumnos.udg.mx>
2023-07-06 20:38:59 -0600

    Add useRef hook and boilerplate basic drawing functions to Canvas

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
-const canvas = ({ width, height }) => {
+const Canvas = ({ width, height }) => {
+
+    const canvasRef = useRef(null)
+
+    const startDrawing = () => {
+    }
+
+    const finishDrawing = () => {
+    }
+
+    const draw = () => {
+    }
+
+    useEffect(() => {
+
+    }, [])
+
     return (
-        <canvas width={width} height={height} style={canvasStyle}></canvas>
+        <canvas
+            width={width}
+            height={height}
+            style={canvasStyle}
+            onMouseDown={startDrawing}
+            onMouseUp={finishDrawing}
+            onMouseMove={draw}
+            ref={canvasRef}></canvas>
     )
 }
 
-export default canvas
+export default Canvas
 
 const canvasStyle = {
     border: "1px solid black"
