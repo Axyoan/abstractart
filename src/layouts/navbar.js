8baab6c1190caccb9026a3ea00500de345c84c7c
Manuel Alexis Gonzalez Romero <75587292+PeterDParker@users.noreply.github.com>
2023-08-12 15:53:54 -0600

    Add files via upload

---
+++ src/layouts/navbar.js
@@ -0,0 +1,28 @@
+import { Navbar, Nav, Container } from "react-bootstrap"
+import { Outlet, Link } from "react-router-dom"
+
+const NavBarExample = () => {
+    return(
+       <>    
+       <Navbar className="navBg" variant="dark" expand="lg">
+        <Container>
+            <Navbar.Brand as={Link} to="/" >Abstractart</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+                <Nav.Link as={Link} to="/" >MainHome</Nav.Link>
+                <Nav.Link as={Link} to="/Canvas">Home</Nav.Link> 
+                <Nav.Link as={Link} to="/signup">SignUp</Nav.Link>
+                <Nav.Link as={Link} to="/galery">Galery</Nav.Link>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>  
+
+        <section>
+            <Outlet></Outlet>
+        </section> 
+       </> 
+    )
+}
+export default NavBarExample
