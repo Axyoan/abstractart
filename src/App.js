89037a2eb4eaf350e82daa0af6e76c866c05d44b
Manuel Alexis Gonzalez Romero <75587292+PeterDParker@users.noreply.github.com>
2023-08-12 15:51:54 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
-import { Routes, Route } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+
 import { Home } from './pages/Home';
 import { SignUp } from './pages/SignUp';
+import MainHome from './pages/MainHome';
+import Galery from './pages/galery';
+import NavBarExample from './layouts/navbar';
 
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
+
+<Routes>
+  <Route path='/' element={ <NavBarExample /> }>
+    <Route index element={ <MainHome /> } />
+    <Route path='galery' element={ <Galery /> } />
+    <Route path='canvas' element={ <Home /> } />
+    <Route path='signup' element={ <SignUp /> } />
+    <Route path='*' element={ <Navigate replace to="/"/> }/>
+  </Route>
+</Routes> 
+
     </div>
   );
 }
