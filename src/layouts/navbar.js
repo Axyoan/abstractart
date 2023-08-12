95269cac9e7ad610d1278ef3ebf91f1e3754640a
Manuel Alexis Gonzalez Romero <75587292+PeterDParker@users.noreply.github.com>
2023-08-12 16:02:57 -0600

    Update navbar.js

--- src/layouts/navbar.js
+++ src/layouts/navbar.js
@@ -4,10 +4,9 @@ import { Outlet, Link } from "react-router-dom"
 const NavBarExample = () => {
     return(
        <>    
-       <Navbar className="navBg" variant="dark" expand="lg">
+       <Navbar className="navBg" variant="light" expand="lg">
         <Container>
             <Navbar.Brand as={Link} to="/" >Abstractart</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
                 <Nav.Link as={Link} to="/" >MainHome</Nav.Link>
