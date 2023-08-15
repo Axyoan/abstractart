ee148aac91e4dd30f9836dd5518ae06b99a5b909
Axyoan Marcelo Castillo <axyoan.marcelo@alumnos.udg.mx>
2023-08-15 13:10:48 -0600

    [Fix] add single quotes to bootstrap import

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import bootstrap from bootstrap/dist/css/bootstrap.min.css
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  
 );
 
 // If you want to start measuring performance in your app, pass a function
