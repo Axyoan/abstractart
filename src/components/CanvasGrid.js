d16903e239691851347a9f06c794b72e1aadb15e
Axyoan Marcelo Castillo <axyoan.marcelo@alumnos.udg.mx>
2023-08-23 19:14:59 -0600

    Implement gallery, load all finished drawings

---
+++ src/components/CanvasGrid.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react'
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+
+const CanvasGrid = ({ count, width, height, canvasesRefs, imagesUrls, isDataReady }) => {
+    const [areImagesReady, setAreImagesReady] = useState(false)
+    const canvasContainerStyle = {
+        position: "relative",
+        left: "0px",
+        right: "0px",
+        marginLeft: "auto",
+        marginRight: "auto",
+        height: height,
+        width: width,
+        backgroundColor: "white",
+        zIndex: "1"
+    }
+
+    useEffect(() => {
+        console.log("grid rendered")
+        const storage = getStorage();
+        imagesUrls.forEach((imageUrl, index) => {
+            const canvas = canvasesRefs.current[index]
+            if (canvas == null) return
+            const canvasContext = canvas.getContext("2d")
+            const firstImg = new Image();
+            const secondImg = new Image();
+            getDownloadURL(ref(storage, imageUrl[0])).then((firstUrl) => {
+                getDownloadURL(ref(storage, imageUrl[1])).then((secondUrl) => {
+                    firstImg.src = firstUrl
+                    secondImg.src = secondUrl
+                    firstImg.onload = function () {
+                        canvasContext.drawImage(firstImg, 0, 0);
+                        secondImg.onload = function () {
+                            canvasContext.drawImage(secondImg, 450, 0);
+                        };
+                    };
+                })
+            })
+        })
+        setAreImagesReady(true)
+    }, [isDataReady])
+
+    return (
+        <>
+            {
+                areImagesReady ?
+                    Array.from({ length: count }, (_, index) =>
+                        <div style={canvasContainerStyle}><canvas
+                            key={index}
+                            width={width}
+                            height={height}
+                            style={canvasStyle}
+                            ref={el => canvasesRefs.current[index] = el}
+                        />
+                        </div>)
+
+                    :
+                    <div>loading</div>
+            }
+        </>
+    )
+}
+
+const canvasStyle = {
+    border: "1px solid black",
+    zIndex: "0"
+}
+export default CanvasGrid
--- src/pages/galery.js
+++ src/pages/galery.js
@@ -1,10 +1,44 @@
-const Galery =() =>{
+import React, { useEffect, useState, useRef } from 'react'
+import { collection, getDocs, getCountFromServer } from "firebase/firestore";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+import { db } from '../firebase-config'
+import CanvasGrid from '../components/CanvasGrid'
+
+const Galery = () => {
+    const [imagesUrls, setImagesUrls] = useState([])
+    const [canvasCount, setCanvasCount] = useState(null)
+    const [isDataReady, setIsDataReady] = useState(false)
+
+    const canvasesRefs = useRef([])
+
+    const loadAllImages = async () => {
+        const queryCountSnapshot = await getCountFromServer(collection(db, "completedDrawingsIds"))
+        const newCanvasCount = queryCountSnapshot.data().count
+        setCanvasCount(newCanvasCount)
+        canvasesRefs.current = canvasesRefs.current.slice(0, newCanvasCount);
+        const querySnapshot = await getDocs(collection(db, "completedDrawingsIds"))
+
+        const newArr = querySnapshot.docs.map((doc) => {
+            const firstId = doc.data().firstId
+            const secondId = doc.data().secondId
+            const firstImageUrlToDownload = 'newDrawings/' + firstId + '.jpg'
+            const secondImageUrlToDownload = 'continuedDrawings/' + secondId + '.jpg'
+            return [firstImageUrlToDownload, secondImageUrlToDownload]
+        })
+        setImagesUrls(newArr)
+        setIsDataReady(true)
+        console.log("all loaded")
+    }
+
+    useEffect(() => {
+        loadAllImages()
+    }, [])
 
     return (
-            <div>
-                <h1>view galery</h1>
-
-            </div>
+        <div>
+            <h1>view galery</h1>
+            <CanvasGrid count={canvasCount} height={500} width={1100} canvasesRefs={canvasesRefs} imagesUrls={imagesUrls} isDataReady={isDataReady} />
+        </div>
 
     )
 }
