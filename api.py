a85e2f4a451d8c7d4af0ba5be6df2c39daeed097
Axyoan Marcelo Castillo <axyoan.marcelo@alumnos.udg.mx>
2023-10-01 15:43:32 -0600

    send necessary data to python script via axios

--- api.py
+++ api.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET'])
 def hello():
+    args = request.args
+    for key in args:
+        print(key)
+        print(args[key])
     return 'Hello, World!'
 
 if __name__ == '__main__':
